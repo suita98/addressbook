10a70e203ac783b6639c481a91b923461ba3dd5b
Yulia <suita98@gmail.com>
2018-11-08 22:08:19 +0300

    Add group_cache and refactor groups length comparing assertion

--- fixture/group.py
+++ fixture/group.py
@@ -20,6 +20,7 @@ class GroupHelper:
         # Submit group creation
         driver.find_element_by_name("submit").click()
         self.return_to_group_page()
+        self.group_cache = None
 
     def fill_group_form(self, group):
         self.change_field_value("group_name", group.name)
@@ -40,6 +41,7 @@ class GroupHelper:
         # Submit deletion
         driver.find_element_by_name("delete").click()
         self.return_to_group_page()
+        self.group_cache = None
 
     def select_first_group(self):
         driver = self.app.driver
@@ -55,6 +57,7 @@ class GroupHelper:
         # Submit modification
         driver.find_element_by_name("update").click()
         self.return_to_group_page()
+        self.group_cache = None
 
     def return_to_group_page(self):
         driver = self.app.driver
@@ -65,13 +68,16 @@ class GroupHelper:
         self.open_groups_page()
         return len(driver.find_elements_by_name("selected[]"))
 
+    group_cache = None
+
     def get_group_list(self):
-        driver = self.app.driver
-        self.open_groups_page()
-        groups = []
-        for element in driver.find_elements_by_css_selector("span.group"):
-            text = element.text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            groups.append(Group(name=text, id=id))
-        return groups
+        if self.group_cache is None:
+            driver = self.app.driver
+            self.open_groups_page()
+            self.group_cache = []
+            for element in driver.find_elements_by_css_selector("span.group"):
+                text = element.text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.group_cache.append(Group(name=text, id=id))
+        return list(self.group_cache)
 
